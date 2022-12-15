d5d6803d8f9be36202f5d6bac529e70cac67f617
Pabearyoga <pabearyoga@gmail.com>
2022-12-16 00:58:53 +0200

    fix login

--- controllers/users/login.js
+++ controllers/users/login.js
@@ -21,7 +21,8 @@ const login = async (req, res, next) => {
     const payload = {
       id: user._id,
     };
-    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "1d" });
+    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "1h" });
+
     await User.findByIdAndUpdate(user._id, { token });
     res.status(200).json({
       token,
