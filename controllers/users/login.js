2d702debf7e3a7b4f4d388eccf7a6f70c09d54a2
Pabearyoga <pabearyoga@gmail.com>
2022-12-16 00:09:13 +0200

    fix login

--- controllers/users/login.js
+++ controllers/users/login.js
@@ -8,11 +8,15 @@ const login = async (req, res, next) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
-    const { subscription } = user;
-    const userPassword = bcrypt.compareSync(password, user.password);
-    if (!user || !userPassword) {
+    if (!user) {
       throw HttpError(401, `Email or password is wrong`);
     }
+    const userPassword = bcrypt.compareSync(password, user.password);
+    if (!userPassword) {
+      throw HttpError(401, `Email or password is wrong`);
+    }
+
+    const { subscription } = user;
 
     const payload = {
       id: user._id,
@@ -27,6 +31,7 @@ const login = async (req, res, next) => {
       },
     });
   } catch (error) {
+    // console.log(error);
     next(error);
   }
 };
