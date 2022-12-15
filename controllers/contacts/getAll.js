119688ceee36a50fbc7ce76da4ccc7053c65a3ea
Pabearyoga <pabearyoga@gmail.com>
2022-12-15 18:41:41 +0200

    add paginafion and filter by "favorite"

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -3,10 +3,17 @@ const { Contact } = require("../../models");
 const getAll = async (req, res, next) => {
   try {
     const { _id } = req.user;
-    const result = await Contact.find({ owner: _id }).populate(
-      "owner",
-      "_id email subscription"
-    );
+    const {
+      page = 1,
+      limit = 200,
+      favorite = { $exists: true || false },
+    } = req.query;
+
+    const skip = (page - 1) * limit;
+    const result = await Contact.find({ owner: _id, favorite: favorite }, "", {
+      skip,
+      limit: Number(limit),
+    }).populate("owner", "_id email subscription");
     res.json(result);
   } catch (error) {
     next(error);
