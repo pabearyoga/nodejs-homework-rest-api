bdc9710301d9eced90c1c6086141dfc6a2bd18bc
Pabearyoga <pabearyoga@gmail.com>
2022-12-22 20:28:00 +0200

    hw05-avatars

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,10 +2,12 @@ const validation = require("./validation");
 const auth = require("./auth");
 const contactIdValidation = require("./contactIdValidation");
 const objIsEmptyValid = require("./objIsEmptyValid");
+const upload = require("./upload");
 
 module.exports = {
   validation,
   auth,
   contactIdValidation,
   objIsEmptyValid,
+  upload,
 };
