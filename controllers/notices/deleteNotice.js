1dd95193db73703c1f9ff36d298e0ae8bdebe847
Pabearyoga <pabearyoga@gmail.com>
2023-01-29 23:18:20 +0200

    fix deleteNotice

--- controllers/notices/deleteNotice.js
+++ controllers/notices/deleteNotice.js
@@ -5,10 +5,12 @@ const deleteNotice = async (req, res, next) => {
   try {
     const { noticeId } = req.params;
     const { _id } = req.user;
+
     const result = await Notice.findByIdAndRemove({
       _id: noticeId,
       owner: _id,
     });
+
     if (result === null) {
       throw HttpError(404, "Not found");
     }
