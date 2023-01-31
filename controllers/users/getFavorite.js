844ec62c3543e9c1fdac88608959af27b4043d60
Pabearyoga <pabearyoga@gmail.com>
2023-02-01 00:31:50 +0200

    add getFavorite

--- controllers/users/getFavorite.js
+++ controllers/users/getFavorite.js
@@ -1,10 +1,33 @@
-// const { User } = require("../../models");
-// const { HttpError } = require("../../helpers");
+const { Notice, User } = require("../../models");
+const { HttpError } = require("../../helpers");
 
-const getFavorite = async (req, res) => {
+const getFavorite = async (req, res, next) => {
   try {
-    console.log("getFavorite");
-  } catch (error) {}
+    const { page = 1, limit = 200 } = req.query;
+    const { _id } = req.user;
+
+    const { favoriteNotices } = await User.findOne({
+      _id: _id,
+    });
+
+    const skip = (page - 1) * limit;
+    const result = await Notice.find(
+      { _id: favoriteNotices.map((noticeId) => noticeId) },
+      "",
+      {
+        skip,
+        limit: Number(limit),
+      }
+    ).populate("owner", "_id email");
+
+    if (!result) {
+      throw HttpError(404, "Not found");
+    }
+
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = getFavorite;
