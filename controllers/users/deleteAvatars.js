ee462938510645bbe0c46e0ae79f58cf24061792
Pabearyoga <pabearyoga@gmail.com>
2023-02-03 16:48:23 +0200

    fix

--- controllers/users/deleteAvatars.js
+++ controllers/users/deleteAvatars.js
@@ -5,12 +5,15 @@ const deleteAvatars = async (req, res, next) => {
   try {
     const { _id } = req.user;
 
-    const result = await User.findByIdAndUpdate(_id, { avatarURl: null });
+    const result = await User.findByIdAndUpdate(
+      { _id: _id },
+      { avatarURL: null }
+    );
 
-    if (result === null) {
+    if (!result) {
       throw HttpError(404, "Not found");
     }
-    res.json(result);
+    res.json({ avatarURL: null });
   } catch (error) {
     next(error);
   }
--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -1,7 +1,6 @@
 const { User } = require("../../models");
 const path = require("path");
 const fs = require("fs/promises");
-// const { avatarImgOptimization } = require("../../helpers");
 
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
@@ -9,14 +8,12 @@ const updateAvatar = async (req, res) => {
   const { path: tempUpload, originalname } = req.file;
   const { _id: id } = req.user;
   const imageName = `${id}_${originalname}`;
-  // const { file } = req;
   try {
-    // await avatarImgOptimization({ file, size: 250 });
     const resultUpload = path.join(avatarsDir, imageName);
     await fs.rename(tempUpload, resultUpload);
-    const avatarURl = path.join("public", "avatars", imageName);
-    await User.findByIdAndUpdate(req.user._id, { avatarURl }); // or create
-    res.json({ avatarURl });
+    const avatarURL = path.join("public", "avatars", imageName);
+    await User.findByIdAndUpdate(req.user._id, { avatarURL }); // or create
+    res.json({ avatarURL });
   } catch (error) {
     await fs.unlink(tempUpload);
     throw error;
